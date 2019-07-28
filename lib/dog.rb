a0e39d6f44c53015c1bcaf8ce526914b69ab2649
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-28 19:51:49 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
 # Add your code here
+class Dog 
+  @@all = []
+  @@clear_all
+  @@print_all
+  
+  def self.print_all
+    @@print_all = puts "#{@@all}"
+  end 
+  def self.all 
+    @@all
+  end 
+  
+  def self.clear_all
+    @@clear_all = @@all.clear 
+  end 
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  attr_reader :save
+  
+  def save=(save)
+    @save = save
+    @@all << self 
+  end 
+  
+end 
+end 
